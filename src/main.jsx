94c804fc3a436b614b1fd6914bd6004b6056edf1
Ancy <ancy.cs24@stellamaryscoe.edu.in>
2026-01-17 21:47:58 +0530

    refactor: Remove ProgressProvider and console logs from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ProgressProvider } from './context/ProgressContext'
-
-console.log('Main.jsx is executing...');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ProgressProvider>
-      <App />
-    </ProgressProvider>
+    <App />
   </StrictMode>,
 )
-console.log('Main.jsx render triggered.');
